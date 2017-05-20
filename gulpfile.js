67e8cf80a38ea50751fe96198aec0da849bb3c1e
Otis Chou <altispec@gmail.com>
2017-05-20 15:03:14 +0800

    improve build script

--- gulpfile.js
+++ gulpfile.js
@@ -7,8 +7,11 @@ const inject = require('gulp-inject')
 const insert = require('gulp-insert')
 const merge = require('merge-stream')
 
+const SRC_PATH = 'src'
+const DIST_PATH = 'dist'
 const JS_PATH = 'src/**/*.js'
 const SASS_PATH = 'src/**/*.sass'
+const PAGES_GLOB = path.join(SRC_PATH, '**/index.html')
 
 // Get target chapter folders recursively
 const getFolders = dir => {
@@ -23,17 +26,17 @@ const getFolders = dir => {
 gulp.task('js', () =>
   gulp.src(JS_PATH, {base: './'})
     .pipe(babel({presets: ['env']}))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(DIST_PATH))
 )
 
 gulp.task('sass', () =>
   gulp.src(SASS_PATH, {base: './'})
       .pipe(sass().on('error', sass.logError))
-      .pipe(gulp.dest('dist'))
+      .pipe(gulp.dest(DIST_PATH))
 )
 
 gulp.task('injectAssetsToPage', () => {
-  const pages = getFolders('src').map(dir => {
+  const pages = getFolders(SRC_PATH).map(dir => {
     const jsSources = gulp.src(path.join(dir, '*.js'), {read: false})
     const cssSources = gulp.src(path.join(dir, '*.+(sass|css)'), {read: false})
 
@@ -55,7 +58,7 @@ gulp.task('injectAssetsToPage', () => {
         starttag: '<!-- inject:css -->',
         transform: filepath => `<link rel="stylesheet" href="${filepath.replace('.sass', '.css')}">`
       }))
-      .pipe(gulp.dest('dist'))
+      .pipe(gulp.dest(DIST_PATH))
   })
 
   return merge(pages)
@@ -63,7 +66,7 @@ gulp.task('injectAssetsToPage', () => {
 
 gulp.task('createHomepage', () =>
   gulp.src('index.html')
-    .pipe(inject(gulp.src('src/**/index.html'), {
+    .pipe(inject(gulp.src(PAGES_GLOB), {
       starttag: '<!-- inject:links -->',
       base: './',
       transform: (filepath, file) => {
@@ -72,13 +75,14 @@ gulp.task('createHomepage', () =>
         return `<li><a href="${filepath}">${title}</a></li>`
       }
     }))
-    .pipe(gulp.dest('dist'))
+    .pipe(gulp.dest(DIST_PATH))
 )
 
 gulp.task('build', ['js', 'sass'])
 gulp.task('watch', () => {
   gulp.watch(JS_PATH, ['js'])
   gulp.watch(SASS_PATH, ['sass'])
-  gulp.watch('src/**/index.html', ['createHomepage', 'injectAssetsToPage'])
+  gulp.watch(PAGES_GLOB, ['createHomepage', 'injectAssetsToPage'])
 })
+
 gulp.task('default', ['build', 'createHomepage', 'injectAssetsToPage'])
